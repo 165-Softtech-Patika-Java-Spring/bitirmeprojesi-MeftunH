764943afe7db5a5bc9e2c794a3cb2fef3438446a
Maftun Hashimli <maftunhashimli@gmail.com>
2022-03-22 06:47:08 +0300

    Init project

---
+++ src/main/java/com/SofttechFinalProject/SofttechFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SofttechFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofttechFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofttechFinalProjectApplication.class, args);
+	}
+
+}
